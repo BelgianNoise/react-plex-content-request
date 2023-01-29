dbd7f3d23174bc3599a6f18aab4ea20abb82c99f
Arthur Joppart <arthur@digita.ai>
2023-01-29 02:37:58 +0100

    add translations for homepage

--- react/src/features/home/Home.tsx
+++ react/src/features/home/Home.tsx
@@ -5,17 +5,19 @@ import { ReactComponent as OverviewSVG } from '../../assets/overview.svg';
 import { ReactComponent as MyRequestsSVG } from '../../assets/my-requests.svg';
 import { ReactComponent as SubmitRequestSVG } from '../../assets/submit-request.svg';
 import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 export function Home() {
   const requests = useAppSelector((state) => state.data.requests);
   const requestsFulfilled = requests.filter((r) => r.status === 'fulfilled').length;
   const latestRequest = requests[0];
+  const { t } = useTranslation();
 
   return (
     <div className={styles.root}>
 
       <h1>
-        {requestsFulfilled} requests have been fulfilled so far!
+        {t('features.home.header-text', { amount: requestsFulfilled })}
       </h1>
 
       <div>
@@ -28,7 +30,7 @@ export function Home() {
             <OverviewSVG />
             <div className={styles.textContainer}>
               <p>
-                Get an overview of all content requests made by you and other people.
+                {t('features.home.overview-card-text')}
               </p>
             </div>
           </div>
@@ -38,7 +40,7 @@ export function Home() {
             <MyRequestsSVG />
             <div className={styles.textContainer}>
               <p>
-                Want to see the status of your content requests or want to delete them? View a list of all your personal content requests.
+                {t('features.home.my-requests-card-text')}
               </p>
             </div>
           </div>
@@ -48,7 +50,7 @@ export function Home() {
             <SubmitRequestSVG />
             <div className={styles.textContainer}>
               <p>
-                Can't find what you are looking for on Plex? Submit a new content request.
+                {t('features.home.submit-request-card-text')}
               </p>
             </div>
           </div>
