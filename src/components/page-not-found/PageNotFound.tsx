81e13108ad0513e96def12cf08bfbe91a3f4702c
Arthur Joppart <arthur@digita.ai>
2023-02-05 20:52:11 +0100

    404 page

--- src/components/page-not-found/PageNotFound.tsx
+++ src/components/page-not-found/PageNotFound.tsx
@@ -1,8 +1,23 @@
 
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import { Button } from '../button/Button';
 import styles from './PageNotFound.module.css';
 
 export function PageNotFound() {
+  const { t } = useTranslation();
+
   return (
-    <div className={styles.root}> 404 </div>
+    <div className={styles.root}>
+      <p className={styles.errorCode}>404</p>
+      <p className={styles.text}>{t('components.page-not-found.text')}</p>
+      <Link to={'/'}>
+        <Button
+          text={t('components.page-not-found.button-text')}
+          buttonStyle={'secondary'}
+          onClick={() => {}}
+        />
+      </Link>
+    </div>
   );
 }
